4c81f1554a7a2ad22ce7c22f49b2d9ebfbe0767e
Mauricio Degregori <degregori2@gmail.com>
2020-08-19 23:00:09 -0500

    added conditional staadded conditional state + integrated facebook and google

--- streak/src/components/Login.jsx
+++ streak/src/components/Login.jsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react";
+
 
 const Login = () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <div>
-            <h1>Login</h1>
+            <h3>LOG IN</h3>
+            {isSignedIn ? (
+                <h3>Signed In</h3>
+            ) : (
+                <h3>Not Signed In </h3>
+            )}
+            
         </div>
     )
 }
